b4a117edc960b38381a0a1e4ac6a3659b3650b61
Chowdog <yangerplato3465@gmail.com>
2022-07-17 20:26:59 +0800

    add global GameManager

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,6 +1,30 @@
 extends Node2D
 
-func _ready():
-	
-	pass
+var playerHp = 20
+var playerMaxHp = 20
+var playerDamage = 3
+var playerDiceOptions = ['attack', 'attack', 'attack', 'attackCrit', 'attackAoe', 'armor', 'potion']
+var playerArmor = 0
+var playerArmorPower = 2
+var playerPotionPower = 2
 
+func setPlayerHp(hp):
+	playerHp = min(playerMaxHp, hp)
+
+func setPlayerMaxHp(hp):
+	playerMaxHp = hp
+
+func setPlayerDamage(damage):
+	playerDamage = damage
+
+func setPlayerDiceOptions(option):
+	playerDiceOptions.append(option)
+
+func setPlayerArmor(armor):
+	playerArmor = armor
+
+func setPlayerArmorPower(power):
+	playerArmorPower = power
+
+func setPlayerPotionPower(power):
+	playerPotionPower = power
